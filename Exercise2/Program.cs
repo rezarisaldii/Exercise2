5e143fab9fd42e596629bd67ea58560cce5e710e
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-04 16:35:56 +0700

    maximum array & input arrayy

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -6,8 +6,55 @@ using System.Threading.Tasks;
 
 namespace Exercise2
 {
-    internal class Program
+    class Program
     {
+        static int InsertionSorting()
+        {
+            Console.Write("\nProgram for sorting a numeric array using Insertion Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] reza = new int[27]; //maximum array
+
+            for (int i = 0; i < 27; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                reza[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //input array
+            Console.Write("Input int array: ");
+            for (int k = 0; k < 27; k++)
+                Console.Write(reza[k] + " ");
+            Console.Write("\n");
+            for (int i = 1; i < 27; i++)
+            {
+                int RR = i;
+                while (RR > 0)
+                {
+                    if (reza[RR - 1] > reza[RR])
+                    {
+                        int temp = reza[RR - 1];
+                        reza[RR - 1] = reza[RR];
+                        reza[RR] = temp;
+                        RR--;
+                    }
+                    else
+                        break;
+                }
+                Console.Write("Iteration " + i.ToString() + ": ");
+                for (int k = 0; k < 27; k++)
+                    Console.Write(reza[k] + " ");
+                Console.Write("\n");
+                //Console.Write("/*** " + (i + 1).ToString() + " numbers from the beginning of the array are input and they are sorted ***/\n");
+            }
+            Console.Write("\n\nThe numbers in ascending orders are given below:\n\n");
+            for (int i =0; i < 27; i++)
+            {
+                Console.Write("Sorted [" + (i + 1).ToString() + "] element");
+                Console.Write(reza[i]);
+                Console.Write("\n");
+            }
+            return 0;
+        }
         static void Main(string[] args)
         {
         }
